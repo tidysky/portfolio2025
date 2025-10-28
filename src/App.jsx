a5c54cb628793894bf0c5631db3458ba5fa64665
norazheng-usyd <zhengnaimeng@hotmail.com>
2025-10-28 15:17:32 +1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Page from './components/page.jsx';
-
+import { useMediaQuery } from 'react-responsive'
 
 
 
@@ -13,13 +13,31 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function App() {
   
-  const handleAnimationComplete = () => {
-  console.log('All letters have animated!');
-};
+
+   const isDesktop = useMediaQuery({ minWidth: 1024 })
 
   return (
     <>
-      <Page />
+     {isDesktop ? (
+        <Page />
+      ) : (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            backgroundColor: '#f5f5f5',
+            color: '#333',
+            padding: '2rem',
+          }}
+        >
+          <h2>For the best visual and interactive experience, please access this website from your computer😊.</h2><br />
+          <p >https://norazhengportfolio2025.vercel.app/</p>
+        </div>
+      )}
     </>
   );
 }
