57e99b50257c02a524ea315ca97612c926e664f4
norazheng-usyd <zhengnaimeng@hotmail.com>
2026-09-12 10:36:23 +1000

    update

--- src/components/MobilePage.jsx
+++ src/components/MobilePage.jsx
@@ -120,7 +120,7 @@ export default function MobilePage(){
                         <h1 className="text-4xl font-sans text-[#b0e86f] Screen2title bold" >HELLO!</h1> 
                         <h1 className="text-6xl font-sans text-[#b0e86f]" >I AM NORA</h1>
                         <span className="text-base text-[#b0e86f] py-6 block" >
-                        AI Engineer <br />Front-End Developer <br />CMS Developer
+                        AI Engineer <br />Designer <br />Developer <br />Multimedia Specialist
                         </span>
                     </div>
                     {/* 图片 */}
